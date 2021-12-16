192c8458bd6561ef741ae169557b6f9cfa92663a
Ferdinando Letizia <ferdinando.letizia@outlook.it>
2021-12-16 15:57:40 +0100

    init+ header

---
+++ src/components/macro/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="py-2 ps-2">
+     <img src="../../assets/img/spotify-logo.png" alt="Spotify"> 
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang='scss' scoped>
+
+header {
+  background-color: #2E3A46;
+}
+
+img {
+  height: 50px;
+}
+
+</style>
---
+++ src/components/macro/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style>
+  main {
+      background-color: #1E2D3B;
+  }
+</style>
